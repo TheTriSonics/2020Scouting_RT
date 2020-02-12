961743cd228ad8526d2d784706f2f8c4dfddf227
Justin Buist <justin.buist@gmail.com>
2020-02-11 19:42:27 -0500

    Refactored/removed

--- lib/signin.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-final FirebaseAuth _auth = FirebaseAuth.instance;
-
-Future<String> signInWithEmail(String email, String pass) async {
-  _auth.signInWithEmailAndPassword(email: email, password: pass);
-  // TODO, check return, redirect
-  return "";
-}
